b6cc0565b4755c4fe51aef7936d63da6f0212ad9
mdalpozzo <marlin.dalpozzo@gmail.com>
2018-01-25 11:04:04 -0800

    Passing find nQueens solution for 1-5; need to debug n = 6. Problem is in Maj and min diag conflicts.

--- src/solvers.js
+++ src/solvers.js
@@ -39,43 +39,74 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = 0; //fixme
+  // var solutionCount = 0; //fixme
   
-  var recursion = function (boardObj, currentRow) {
-    // for every col
-    for (var i = 0; i < n; i++) {
-      // reset currentRow to [0]
-      var reset = new Array(n);
-      reset.fill(0);
-      boardObj.set(currentRow, reset);
-      // put rook in next place
-      boardObj.togglePiece(currentRow, i);
-      // if conflict, undo
-      if (boardObj.hasColConflictAt(i)) {
-        boardObj.togglePiece(currentRow, i);
-      // else if we're at the end, count++ and return from function call
-      } else if (currentRow === n - 1) {
-        solutionCount++;
-        return;
-      // otherwise recurse on a copy of current board and start from next row
-      } else {
-        recursion(new Board(boardObj.rows()), currentRow + 1);
-      }
-    }
-  };
+  // var recursion = function (boardObj, currentRow) {
+  //   // for every col
+  //   for (var i = 0; i < n; i++) {
+  //     // reset currentRow to [0]
+  //     var reset = new Array(n);
+  //     reset.fill(0);
+  //     boardObj.set(currentRow, reset);
+  //     // put rook in next place
+  //     boardObj.togglePiece(currentRow, i);
+  //     // if conflict, undo
+  //     if (boardObj.hasColConflictAt(i)) {
+  //       boardObj.togglePiece(currentRow, i);
+  //     // else if we're at the end, count++ and return from function call
+  //     } else if (currentRow === n - 1) {
+  //       solutionCount++;
+  //       return;
+  //     // otherwise recurse on a copy of current board and start from next row
+  //     } else {
+  //       recursion(new Board(boardObj.rows()), currentRow + 1);
+  //     }
+  //   }
+  // };
   
-  recursion(new Board({n: n}), 0);
+  // recursion(new Board({n: n}), 0);
   
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  // console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = new Board({n: n});
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  for (var q = 0; q < n; q++) {
+    if (n === 6) {
+      debugger;
+    }
+    // clean board
+    solution = new Board({n: n});
+    var queenCount = 0;
+    // place 1st row queen
+    solution.togglePiece(0, q);
+    queenCount++;
+    //outerloop for rows
+    for (var i = 1; i < n; i++) {
+      //innerloop for columns
+      for (var j = 0; j < n; j++) {
+        solution.togglePiece(i, j);
+        queenCount++;
+        
+        if (solution.hasAnyQueenConflictsOn(i, j) === true) {
+          solution.togglePiece(i, j);
+          queenCount--;
+        } else {
+          break;
+        }
+      }
+    }
+    // check queenCount
+    if (queenCount === n) {
+      break;
+    }
+  }
+
+  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution.rows()));
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
