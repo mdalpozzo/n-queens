a9f1766957e47d471743d9993e32a148e38b7781
mdalpozzo <marlin.dalpozzo@gmail.com>
2018-01-24 17:41:16 -0800

    working on nrook solution

--- src/solvers.js
+++ src/solvers.js
@@ -16,15 +16,27 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  // get empty board
+  var solution = new Board({n: n}); //fixme
+  
+  //outerloop for rows
+  for (var i = 0; i < n; i++) {
+    //innerloop for columns
+    for (var j = 0; j < n; j++) {
+      if (solution.hasColConflictAt(j) === false) {
+        solution.togglePiece(i, j);
+        break;
+      }
+    }
+  }
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 1; //fixme
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
